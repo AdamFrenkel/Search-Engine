c09583e3ad50211124f93a6475a77c29afc7cbc8
adamfrenkel <adam.frenkel@yahoo.com>
2021-04-11 17:50:52 -0400

    Just getting started BeH

---
+++ DataStructures/project/stage4/src/main/java/edu/yu/cs/com1320/project/impl/MinHeapImpl.java
@@ -0,0 +1,32 @@
+package edu.yu.cs.com1320.project.impl; //hmmm... maybe in wrong spot
+
+import edu.yu.cs.com1320.project.MinHeap;
+
+public class MinHeapImpl<E extends Comparable<E>> extends MinHeap<E> {
+
+    @Override
+    public void reHeapify(E element) {
+        int k = this.getArrayIndex(element);
+        if(this.isGreater(k,k/2)){
+            upHeap(k);
+        }
+        if(this.isGreater(2*k,k) || this.isGreater(2*k+1,k)){
+            downHeap(k);
+        }
+    }
+
+    @Override
+    protected int getArrayIndex(E element) {
+        for(int i = 0; i<elements.length; i++){
+            if (elements[i] == element){
+                return i;
+            }
+        }
+        throw new IllegalArgumentException("element not in array");
+    }
+
+    @Override
+    protected void doubleArraySize() {
+
+    }
+}
---
+++ DataStructures/project/stage4/src/test/java/edu/yu/cs/com1320/project/stage4/impl/DocumentStoreImplTest.java
@@ -0,0 +1,7 @@
+package edu.yu.cs.com1320.project.stage4.impl;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DocumentStoreImplTest {
+
+}
