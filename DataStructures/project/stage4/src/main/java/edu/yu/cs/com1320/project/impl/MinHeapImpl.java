3faa4c17e2fe919e5a6c10243af59b1208f99cd8
adamfrenkel <adam.frenkel@yahoo.com>
2021-04-13 16:19:02 -0400

    Starting to test

--- DataStructures/project/stage4/src/main/java/edu/yu/cs/com1320/project/impl/MinHeapImpl.java
+++ DataStructures/project/stage4/src/main/java/edu/yu/cs/com1320/project/impl/MinHeapImpl.java
@@ -5,22 +5,32 @@ import edu.yu.cs.com1320.project.MinHeap;
 import java.lang.reflect.Array;
 
 public class MinHeapImpl<E extends Comparable<E>> extends MinHeap<E> {
-
+    public MinHeapImpl(){
+        elements = (E[]) new Comparable[5];
+    }
     @Override
     public void reHeapify(E element) {
         int k = this.getArrayIndex(element);
         if(this.isGreater(k/2,k)){
             upHeap(k);
         }
-        if(this.isGreater(k,2*k) || this.isGreater(k,2*k+1)){
-            downHeap(k);
+        else{
+            if(2*k+1<= elements.length) {
+                if (this.isGreater(k, 2 * k) || this.isGreater(k, 2 * k + 1)) {
+                    downHeap(k);
+                }
+            }
         }
     }
 
     @Override
     protected int getArrayIndex(E element) {
+        if(element == null){
+            throw new IllegalArgumentException("null element");
+        }
         for(int i = 0; i<elements.length; i++){
-            if (elements[i] == element){
+            if(elements[i] != null)
+            if (elements[i].equals(element)){
                 return i;
             }
         }
@@ -30,7 +40,7 @@ public class MinHeapImpl<E extends Comparable<E>> extends MinHeap<E> {
     @Override
     protected void doubleArraySize() {
         E[] elementsCopy = elements;
-        elements = (E[]) new Object[elements.length *2];
+        elements = (E[]) new Comparable[elements.length *2];
         for(int i = 0; i<elementsCopy.length; i++){
             elements[i] = elementsCopy[i];
         }
